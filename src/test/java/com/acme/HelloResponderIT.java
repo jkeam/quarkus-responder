11a716f104f89aa9bd888d636165e60c36f6c500
Jon Keam <jpkeam@gmail.com>
2021-11-29 14:46:41 -0500

    refactor name of integration test

--- src/test/java/com/acme/HelloResponderIT.java
+++ src/test/java/com/acme/HelloResponderIT.java
@@ -3,7 +3,7 @@ package com.acme;
 import io.quarkus.test.junit.QuarkusIntegrationTest;
 
 @QuarkusIntegrationTest
-public class HelloResponderIntegrationTest extends HelloResponderTest {
+public class HelloResponderIT extends HelloResponderTest {
   // run same test as HelloResponderTest
   // but using the native image bc of the QuarkusIntegrationTest annotation
 }
